364c80dc39b694ef9a8d64136d46937b2ac170d7
Eric Treadway <eric.s.treadway@gmail.com>
2021-04-19 09:49:18 -0500

    removed static image links in db

--- db.js
+++ db.js
@@ -2,27 +2,27 @@ module.exports = [
     {
         name: 'Doodle',
         breed: 'hound',
-        image: 'https://img.webmd.com/dtmcms/live/webmd/consumer_assets/site_images/article_thumbnails/other/dog_cool_summer_slideshow/1800x1200_dog_cool_summer_other.jpg'
+        image: ''
     },
     {
         name: 'Burrow',
         breed: 'corgi',
-        image: 'temhttps://img.webmd.com/dtmcms/live/webmd/consumer_assets/site_images/article_thumbnails/other/dog_cool_summer_slideshow/1800x1200_dog_cool_summer_other.jpgp'
+        image: ''
     },
     {
         name: 'Ella',
         breed: 'hound',
-        image: 'https://img.webmd.com/dtmcms/live/webmd/consumer_assets/site_images/article_thumbnails/other/dog_cool_summer_slideshow/1800x1200_dog_cool_summer_other.jpg'
+        image: ''
     },
     {
         name: 'Chloe',
         breed: 'hound',
-        image: 'https://img.webmd.com/dtmcms/live/webmd/consumer_assets/site_images/article_thumbnails/other/dog_cool_summer_slideshow/1800x1200_dog_cool_summer_other.jpg'
+        image: ''
     },
     {
         name: 'Fluffy',
         breed: 'poodle',
-        image: 'https://img.webmd.com/dtmcms/live/webmd/consumer_assets/site_images/article_thumbnails/other/dog_cool_summer_slideshow/1800x1200_dog_cool_summer_other.jpg'
+        image: ''
     },
 
 ]
